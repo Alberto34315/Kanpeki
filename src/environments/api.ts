0fe9556c3a65788578bfc57809500dbd87c4ea39
Alberto34315 <ruizserranoalberto5@gmail.com>
2022-05-03 14:09:26 +0200

    Prueba email

---
+++ src/environments/api.ts
@@ -0,0 +1,12 @@
+export const api = {
+  apiUrl: "http://cf1.hostemy.com:30008/",
+  apiUrllocal: "http://localhost:8080/",
+  error: "error",
+  kanpeki: "kanpeki/",
+  category: "categories",
+  question: "questions",
+  result: "results",
+  user: "users",
+  word: "words",
+  files:"files"
+};
